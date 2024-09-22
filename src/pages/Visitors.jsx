5618b84ed25701ed33dcaa5f00d2a9153eafb6b5
Mohammad Yunus <mohammad.yunus@codvo.ai>
2024-09-22 12:49:21 +0530

    color change

--- src/pages/Visitors.jsx
+++ src/pages/Visitors.jsx
@@ -24,7 +24,7 @@ const Visitors = () => {
   }, []);
   return (
     <div
-      className="relative  bg-cover bg-center flex items-center justify-start flex-col mt-20 overflow-y-auto "
+      className="relative  bg-cover bg-center flex items-center justify-start flex-col mt-20 overflow-y-auto text-white "
       style={{
         backgroundImage:
           'url("https://images.unsplash.com/photo-1559919304-00f415bab202?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)',
